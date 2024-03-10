d242df4cc3dac2ac39d6b6585147a7a31f642f8a
Pedro Molina <pealmo98@gmail.com>
2024-03-10 17:24:32 -0500

    <feat>: <The main functions of a calculator are defined>

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,14 @@
-hola comit de mangel 
+def suma(num1, num2):
+    return num1 + num2
+
+def resta(num1, num2):
+    return num1 - num2
+
+def multiplicacion(num1, num2):
+    return num1 * num2
+
+def division(num1, num2):
+    if num2 == 0:
+        return "Error: No se puede dividir por cero"
+    else:
+        return num1 / num2
